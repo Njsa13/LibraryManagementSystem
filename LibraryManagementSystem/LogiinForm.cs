d6f879f823ce9e5a59750ff2b64ae515f9f21dc2
Njsa13 <najib.sauqi@students.amikom.ac.id>
2022-12-30 11:13:01 +0700

    Add project files.

---
+++ LibraryManagementSystem/LogiinForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LibraryManagementSystem
+{
+    public partial class LogiinForm : Form
+    {
+        public LogiinForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MYBD db = new MYBD();
+
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `username`= @usn AND `password`= @pass", db.getConnection());
+
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = username;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = password;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0)
+            {
+                GenresForm genform = new GenresForm();
+                genform.Tag = this;
+                genform.Show(this);
+                Hide();
+            }
+            else
+            {
+                if (username.Trim().Equals(""))
+                {
+                    MessageBox.Show("Enter Username to Login", "Empty Username",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (password.Trim().Equals(""))
+                {
+                    MessageBox.Show("Enter Password to Login", "Empty Password",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Username or Password", "Wrong Data",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
